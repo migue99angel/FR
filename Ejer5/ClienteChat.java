8fe401b729a3ddcce00a81802e04b3e96f3c838c
Francisco Domínguez <d3vcho@protonmail.com>
2019-11-26 16:48:17 +0100

    Añade clases para el ejercicio 5

---
+++ Ejer5/ClienteChat.java
@@ -0,0 +1,115 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Cliente {
+
+    private Socket socketConexion;
+    private ServerSocket socketServidor;
+    private DataInputStream bufferEntrada = null;
+    private DataOutputStream bufferSalida = null;
+    Scanner teclado = new Scanner(System.in);
+    final String COMANDO_TERMINACION = "salir()";
+    int port = 42069;
+    String ip = localhost;
+
+    // Función para levantar la conexión desde el cliente
+    public void levantarConexion() {
+        try {
+            socket = new Socket(ip, port);
+            System.out.println("Conectado a :" + socket.getInetAddress().getHostName());
+        } catch (Exception e) {
+            System.out.println("Excepción al levantar conexión: " + e.getMessage());
+            System.exit(0);
+        }
+    }
+
+    // Abrimos los flujos desde el cliente
+    public void abrirFlujos() {
+        try {
+            bufferEntrada = new DataInputStream(socketConexion.getInputStream());
+            bufferSalida = new DataOutputStream(socketConexion.getOutputStream());
+            bufferSalida.flush();
+
+        } catch (IOException e) {
+            System.err.println("Error en la apertura de flujos");
+        }
+    }
+
+    // Enviamos el contenido de los flujos
+    public void enviar(String s) {
+        try {
+            bufferSalida.writeUTF(s);
+            bufferSalida.flush();
+        } catch (IOException e) {
+            System.err.println("Error al enviar");
+        }
+    }
+
+    // Cerramos la conexión desde el cliente
+    public void cerrarConexion() {
+        try {
+            bufferEntrada.close();
+            bufferSalida.close();
+            socketConexion.close();
+
+            System.out.println("Conexión terminada");
+        } catch (IOException e) {
+            System.out.println("IOException on cerrarConexion()");
+        }
+    }
+
+    // Ejecutamos una nueva conexión con la clase Thread
+    public void ejecutarConexion() {
+        Thread hilo = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    levantarConexion();
+                    abrirFlujos();
+                    recibirDatos();
+                } finally {
+                    cerrarConexion();
+                }
+            }
+        });
+        hilo.start();
+    }
+
+    // Recibimos los datos en los flujos
+    public void recibirDatos() {
+        String st = "";
+        try {
+            do {
+                st = (String) bufferDeEntrada.readUTF();
+                System.out.println("\n[Servidor] => " + st);
+                System.out.print("\n[Tú] => ");
+            } while (!st.equals(COMANDO_TERMINACION));
+        } catch (IOException e) {
+            cerrarConexion();
+        }
+    }
+
+    // Escribimos nuestros datos y los enviamos
+    public void escribirDatos() {
+        String entrada = "";
+        while (true) {
+            System.out.print("[Usted] => ");
+            entrada = teclado.nextLine();
+            if(entrada.length() > 0)
+                enviar(entrada);
+        }
+    }
+
+    // Método principal
+    public static void main(String[] argumentos) {
+        Cliente cliente = new Cliente();
+        Scanner escaner = new Scanner(System.in);
+
+        cliente.ejecutarConexion();
+        cliente.escribirDatos();
+    }
+}
---
+++ Ejer5/ServidorChat.java
@@ -0,0 +1,117 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+
+public class Servidor {
+
+    private Socket socketConexion;
+    private ServerSocket socketServidor;
+    private DataInputStream bufferEntrada = null;
+    private DataOutputStream bufferSalida = null;
+    Scanner escaner = new Scanner(System.in);
+    final String COMANDO_TERMINACION = "salir()";
+    int port = 42069;
+
+    // Función para intentar establecer una conexión
+    public void levantarConexion() {
+        try {
+            socketServidor = new ServerSocket(port);
+            socketConexion = socketServidor.accept();
+
+            System.out.println("Conexión establecida");
+
+        } catch (IOException e) {
+            System.err.println("Error al escuchar en el puerto " + port);
+        }
+    }
+
+    // Lee los flujos y verifica que son correctos
+    public void flujos() {
+        try {
+            bufferEntrada = new DataInputStream(socketConexion.getInputStream());
+            bufferSalida = new DataOutputStream(socketConexion.getOutputStream());
+            bufferSalida.flush();
+
+        } catch (IOException e) {
+            System.out.println("Error en la apertura de flujos");
+        }
+    }
+
+    // Recibimos datos de entrada hasta que introducimos salir()
+    public void recibirDatos() {
+        String st = "";
+        try {
+            do {
+                st = (String) bufferEntrada.readUTF();
+                System.out.println("\n[Cliente] => " + st);
+                System.out.println("\n[Tú] => ");
+            } while (!st.equals(COMANDO_TERMINACION));
+        } catch (IOException e) {
+            cerrarConexion();
+        }
+    }
+
+
+    // Enviamos nuestro texto
+    public void enviar(String s) {
+        try {
+            bufferSalida.writeUTF(s);
+            bufferSalida.flush();
+        } catch (IOException e) {
+            System.err.println("Error al enviar");
+        }
+    }
+
+    // Escribimos nuestro texto
+    public void escribirDatos() {
+        while (true) {
+            System.out.print("[Tú] => ");
+            enviar(escaner.nextLine());
+        }
+    }
+
+    // Cerramos la conexión
+    public void cerrarConexion() {
+        try {
+            bufferEntrada.close();
+            bufferSalida.close();
+            socketConexion.close();
+
+        } catch (IOException e) {
+          System.err.print("Error al intentar cerrar la conexión");
+        }
+    }
+
+    // Ejecutamos una nueva conexión con la clase Thread
+    public void ejecutarConexion() {
+        Thread nuevoHilo = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        levantarConexion(port);
+                        flujos();
+                        recibirDatos();
+                    } finally {
+                        cerrarConexion();
+                    }
+                }
+            }
+        });
+
+        nuevoHilo.start();
+    }
+
+    // Método principal
+    public static void main(String[] args){
+        Servidor s = new Servidor();
+        Scanner sc = new Scanner(System.in);
+
+        s.ejecutarConexion(port);
+        s.escribirDatos();
+    }
+}
